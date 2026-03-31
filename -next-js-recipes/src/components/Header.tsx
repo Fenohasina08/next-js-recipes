49f1704609affe6f711607f2ba85d6969062d7f6
Fenohasina08 <fenohasina.ra_fanomezana@student.passerellesnumerIques.org>
2026-03-31 19:26:46 +0300

    feat: make the header at the same stage

--- -next-js-recipes/src/components/Header.tsx
+++ -next-js-recipes/src/components/Header.tsx
@@ -6,11 +6,11 @@ interface HeaderProps {
 
 export default function Header({ onReverse }: HeaderProps) {
   return (
-    <header className="header">
-      <h1>Recipe book</h1>
-      <button className="reverse-btn" onClick={onReverse}>
-        Reverse order
-      </button>
+    <header className="header flex items-center justify-between">
+        <h1>Recipe book</h1>
+            <button className="reverse-btn" onClick={onReverse}>
+                Reverse order
+            </button>
     </header>
   );
 }
