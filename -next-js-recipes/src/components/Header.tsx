115c7098227c7cd8ffbddcbf6860c250fe08cec3
Fenohasina08 <fenohasina.ra_fanomezana@student.passerellesnumerIques.org>
2026-03-31 19:28:27 +0300

    feat: add 8vh for header

--- -next-js-recipes/src/components/Header.tsx
+++ -next-js-recipes/src/components/Header.tsx
@@ -6,7 +6,7 @@ interface HeaderProps {
 
 export default function Header({ onReverse }: HeaderProps) {
   return (
-    <header className="header flex items-center justify-between">
+    <header className="header flex items-center justify-between h-[8vh] bg-amber-50 text-black">
         <h1>Recipe book</h1>
             <button className="reverse-btn" onClick={onReverse}>
                 Reverse order
