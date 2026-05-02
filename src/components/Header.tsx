b79beef705c9cfc70e19d112df48657e9a0285b0
Fenohasina08 <fenohasina.ra_fanomezana@student.passerellesnumerIques.org>
2026-05-02 19:07:09 +0300

    refactor(header):delete the current heade

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,14 +6,8 @@ interface HeaderProps {
 
 export default function Header({ onReverse }: HeaderProps) {
   return (
-    <header className="header flex items-center justify-between h-[8vh] bg-amber-50 text-black">
-        <h1>Recipe book</h1>
-            <button
-            className="reverse-btn bg-amber-500 text-white px-4 py-2 rounded-lg font-medium shadow-md hover:bg-amber-600 transition"
-            onClick={onReverse}
-            >                
-            Reverse order
-            </button>
-    </header>
+   <header className="flex items-center justify-center h-[8vh] bg-amber-50 text-black">
+  <p className="text-center">Recipe book</p>
+</header>
   );
 }
